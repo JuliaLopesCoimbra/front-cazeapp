e6f8c18f531ce477b1cb441d8d6dd82003b9211b
JuliaLopesCoimbra <juliacristinalopes2607@gmail.com>
2026-01-15 17:24:58 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // <--- ADICIONE ESTA LINHA AQUI
-  images: {
-    unoptimized: true, // <--- OBRIGATÓRIO PARA EXPORT ESTÁTICO SE USAR <Image />
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "n1-bucket-s3.s3.sa-east-1.amazonaws.com",
-        pathname: "/**",
-      },
-    ],
-  },
-  turbopack: {
-    resolveAlias: {
-      "@": "./",
-    },
-    resolveExtensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "n1-bucket-s3.s3.sa-east-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  // Configuração do Turbopack para resolução de módulos
+  turbopack: {
+    resolveAlias: {
+      "@": "./",
+    },
+    resolveExtensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
 };
 
 export default nextConfig;
+
